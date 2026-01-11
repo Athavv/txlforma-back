f2ca0eca893993403518e2f08c932cd509b28bf4
Aathavan Thevakumar <aathavanthevakumar@gmail.com>
2026-01-11 02:21:22 +0100

    host the app fix

--- src/main/java/mmi/osaas/txlforma/config/SecurityConfig.java
+++ src/main/java/mmi/osaas/txlforma/config/SecurityConfig.java
@@ -118,7 +118,8 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://localhost:3000",
                 "http://127.0.0.1:5173",
-                "http://127.0.0.1:3000"
+                "http://127.0.0.1:3000",
+                "https://txlforma.vercel.app"
         ));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
